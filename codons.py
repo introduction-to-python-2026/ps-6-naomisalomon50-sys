3b9d7162d9f2bd832af303674130a1525f62b6a5
naomisalomon50-sys <naomi.salomon50@gmail.com>
2025-11-29 20:46:23 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -2,3 +2,12 @@ def create_codon_dict(file_path):
     pass # Replace the pass with your code
 
 
+dict = {}
+split_row = []
+for row in rows:
+  split_row = row.strip().split('\t')
+  codon = split_row[0]
+  amino_acid = split_row[2]
+  dict[codon] = amino_acid
+  
+print(dict)
